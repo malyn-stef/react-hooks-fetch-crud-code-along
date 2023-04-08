d0b42fa13b4e7abeedb8b5d9771cf741187be33e
+ <+>
2023-04-07 21:33:23 -0600

    code along

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,14 +1,39 @@
 import React from "react";
 
-function Item({ item }) {
+function Item({ item, onUpdateItem, onHandleDelete }) {
+
+  function handleAddToCartClick() {
+    fetch(`//localhost:4000/items/${item.id}`, {
+      method: "PATCH",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ isInCart: !item.isInCart })
+    })
+      .then(res => res.json())
+      .then(updatedItem => onUpdateItem(updatedItem))
+
+  }
+
+  function handleDelete() {
+    fetch(`//localhost:4000/items/${item.id}`, {
+      method: "DELETE",
+    })
+      .then(res => res.json())
+      .then(() => onHandleDelete(item))
+
+  }
+
   return (
     <li className={item.isInCart ? "in-cart" : ""}>
       <span>{item.name}</span>
       <span className="category">{item.category}</span>
-      <button className={item.isInCart ? "remove" : "add"}>
-        {item.isInCart ? "Remove From" : "Add to"} Cart
+      <button
+        className={item.isInCart ? "remove" : "add"}
+        onClick={handleAddToCartClick}>
+        {item.isInCart ? "Remove From Cart" : "Add to Cart"}
       </button>
-      <button className="remove">Delete</button>
+      <button className="remove" onClick={handleDelete}>Delete</button>
     </li>
   );
 }
--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,11 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-function ItemForm() {
+function ItemForm({ onAddItem }) {
   const [name, setName] = useState("");
   const [category, setCategory] = useState("Produce");
 
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    const newItem = {
+      name: name,
+      category: category,
+      isInCart: false
+    }
+    fetch('http://localhost:4000/items', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newItem)
+    })
+      .then(res => res.json())
+      .then(newItem => onAddItem(newItem))
+  }
+
+
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
         <input
@@ -29,7 +50,7 @@ function ItemForm() {
         </select>
       </label>
 
-      <button type="submit">Add to List</button>
+      <button type="submit"  >Add to List</button>
     </form>
   );
 }
